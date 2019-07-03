242336e237c1779cca1119057874ab1d7cff7158
pholmes10 <pholmes10@gmail.com>
2019-07-03 15:08:55 -0600

    navigation controller and multiple VCs

---
+++ Bankhouse/LatestNewsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LatestNewsViewController.swift
+//  Bankhouse
+//
+//  Created by Patrick Holmes on 7/3/19.
+//  Copyright © 2019 Patrick Holmes. All rights reserved.
+//
+
+import UIKit
+
+class LatestNewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
