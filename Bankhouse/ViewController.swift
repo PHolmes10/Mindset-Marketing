2845023f520dbaf7c518ce5e4808a51b77973ca6
pholmes10 <pholmes10@gmail.com>
2019-07-15 13:46:50 -0600

    change out images for different sections of the app, add launch screen with logo

--- Bankhouse/ViewController.swift
+++ Bankhouse/ViewController.swift
@@ -27,26 +27,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        self.view.backgroundColor = UIColor (red: 45.0/255.0, green: 45.0/255.0, blue: 45.0/255.0, alpha: 1.0)
+
         howWeWorkButton.setBackgroundImage(UIImage(named: "howWeWork"), for: .normal)
-        howWeWorkButton.setTitle("How We Work", for: .normal)
+//        howWeWorkButton.setTitle("How We Work", for: .normal)
 //        howWeWorkButton.titleLabel?.backgroundColor = UIColor.white
 
         ourTeachingsButton.setBackgroundImage(UIImage(named: "ourTeachings2"), for: .normal)
-        ourTeachingsButton.setTitle("Our Teachings", for: .normal)
-        ourTeachingsButton.titleLabel?.textAlignment = .center
+//        ourTeachingsButton.setTitle("Our Teachings", for: .normal)
+//        ourTeachingsButton.titleLabel?.textAlignment = .center
 
         
         latestNewsButton.setBackgroundImage(UIImage(named: "latestNews"), for: .normal)
-        latestNewsButton.setTitle("Latest News", for: .normal)
+//        latestNewsButton.setTitle("Latest News", for: .normal)
         
         podcastButton.setBackgroundImage(UIImage(named: "podcast"), for: .normal)
-        podcastButton.setTitle("Podcast", for: .normal)
+//        podcastButton.setTitle("Podcast", for: .normal)
         
-        upcomingEventsButton.setBackgroundImage(UIImage(named: "upcomingEvents"), for: .normal)
-        upcomingEventsButton.setTitle("Upcoming Events", for: .normal)
+        upcomingEventsButton.setBackgroundImage(UIImage(named: "upcomingEvents2"), for: .normal)
+//        upcomingEventsButton.setTitle("Upcoming Events", for: .normal)
         
         membersAreaButton.setBackgroundImage(UIImage(named: "membersArea"), for: .normal)
-        membersAreaButton.setTitle("Members Area", for: .normal)
+//        membersAreaButton.setTitle("Members Area", for: .normal)
         
         
 //        howWeWorkButton.imageView?.contentMode = .scaleAspectFill
